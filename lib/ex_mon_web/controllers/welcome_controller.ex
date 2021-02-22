a6e7a38828bed7b8ebce992a780145358581cf38
Pedro <pdrfiuza@gmail.com>
2021-02-21 23:42:30 -0300

    rota e migration

---
+++ lib/ex_mon_web/controllers/welcome_controller.ex
@@ -0,0 +1,36 @@
+defmodule ExMonWeb.WelcomeController do
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  use ExMonWeb, :controller
+
+  def index(conn, _params) do
+    text(conn, "Welcome to the ExMon API.")
+  end
+
+end
